9e3264b770c8f95933eb165b7e603606b5d4b38b
Evan <evan.c405@stu.nebo.edu>
2022-03-03 14:21:00 -0700

    FInished animations and did some coding

---
+++ VrPrototype1 copy/Assets/Scripts/Hand.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private float speed;
+    private string animatorGripParam = "Grip";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripTarget != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+    }
+}
